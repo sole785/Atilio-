1bf93f6e3bd6542f0d2a880b262a455990da1aab
sole785 <sole785@gmail.com>
2018-11-23 00:05:02 -0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+<<<<<<< HEAD
             $table->boolean('isActive') ->default(false);
+=======
+>>>>>>> parent of 0532eef... Update 2014_10_12_000000_create_users_table.php
             $table->string('name');
             $table->integer('cuit')->unique();
             $table->string('razon_social');
